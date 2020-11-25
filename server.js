05845492e0295f2f8fdf18c94eee74907dddd97f
Paul Varner <fuzzyray@gmail.com>
2020-11-25 17:42:10 -0600

    Add ability to run on https and reformat

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 require('dotenv').config();
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const expect      = require('chai').expect;
-const cors        = require('cors');
+const https = require('https');
+const path = require('path');
+const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
+const expect = require('chai').expect;
+const cors = require('cors');
 
-const fccTestingRoutes  = require('./routes/fcctesting.js');
-const apiRoutes         = require('./routes/api.js');
-const runner            = require('./test-runner');
+const fccTestingRoutes = require('./routes/fcctesting.js');
+const apiRoutes = require('./routes/api.js');
+const runner = require('./test-runner');
 
 const app = express();
 
@@ -14,11 +17,11 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(cors({origin: '*'})); //For FCC testing purposes only
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
 //Index page (static HTML)
 app.route('/')
-  .get(function (req, res) {
+  .get(function(req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
   });
 
@@ -27,7 +30,7 @@ fccTestingRoutes(app);
 
 // User routes
 apiRoutes(app);
-    
+
 //404 Not Found Middleware
 app.use(function(req, res, next) {
   res.status(404)
@@ -35,14 +38,29 @@ app.use(function(req, res, next) {
     .send('Not Found');
 });
 
+//Setup server and use SSL if enabled
+let server;
+let PORT;
+if (!!process.env.ENABLE_SSL) {
+  const certOptions = {
+    key: fs.readFileSync(path.resolve('certs/server.key')),
+    cert: fs.readFileSync(path.resolve('certs/server.crt')),
+  };
+
+  server = https.createServer(certOptions, app);
+  PORT = process.env.PORT || 8443;
+} else {
+  server = app;
+  PORT = process.env.PORT || 3000;
+}
+
 //Start our server and tests!
-const PORT = process.env.PORT || 3000
-app.listen(PORT, function () {
-  console.log("Listening on port " + PORT);
+const listener = server.listen(PORT, function() {
+  console.log(`Listening on port ${listener.address().port}`);
   // process.env.NODE_ENV='test'
-  if (process.env.NODE_ENV==='test') {
+  if (process.env.NODE_ENV === 'test') {
     console.log('Running Tests...');
-    setTimeout(function () {
+    setTimeout(function() {
       try {
         runner.run();
       } catch (error) {
