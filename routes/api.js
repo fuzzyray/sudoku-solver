0d913d87f984eccf4fdcb71e39119a3fbbf559c3
Paul Varner <fuzzyray@gmail.com>
2020-11-29 13:18:22 -0600

    Implement /api/solve route

--- routes/api.js
+++ routes/api.js
@@ -1,28 +1,47 @@
 /*
-*
-*
-*       Complete the API routing below
-*
-*
-*/
+ *
+ *
+ *       Complete the API routing below
+ *
+ *
+ */
 
 'use strict';
 
 const SudokuSolver = require('../controllers/sudoku-solver.js');
 
-module.exports = function (app) {
-  
-  let solver = new SudokuSolver();
+module.exports = function(app) {
+
+  const solver = new SudokuSolver();
 
   app.route('/api/check')
     .post((req, res) => {
-      console.log(req.body)
-      res.json({error: 'not implemented'})
+      console.log(req.body);
+      res.json({error: 'not implemented'});
     });
-    
+
   app.route('/api/solve')
     .post((req, res) => {
-      console.log(req.body)
-      res.json({error: 'not implemented'})
+      if (!!req.body.puzzle) {
+        const puzzleString = req.body.puzzle;
+        if (solver.isValidPuzzleString(puzzleString)) {
+          solver.setBoardFromString(puzzleString);
+          const solution = solver.solve();
+          const solutionErrorRegEx = /timeout|invalid/;
+          res.json(solutionErrorRegEx.test(solution)
+            ? {error: 'Puzzle cannot be solved'}
+            : {solution: solution});
+        } else {
+          if (!solver.isValidLength(puzzleString)) {
+            res.json({error: 'Expected puzzle to be 81 characters long'});
+          } else if (!solver.isValidCharacters(puzzleString)) {
+            res.json({error: 'Invalid characters in puzzle'});
+          } else {
+            res.json({error: 'Unknown error, this is a bug'});
+          }
+        }
+      } else {
+        res.json({error: 'Required field missing'});
+      }
     });
 };
