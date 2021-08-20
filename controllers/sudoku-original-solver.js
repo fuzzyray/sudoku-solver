d7f2490efab49f86060fd080b80a9d4ac4044d38
Paul Varner <fuzzyray@gmail.com>
2021-08-19 19:54:33 -0500

    Replace original solver with a heuristic brute force recursive algorithm

---
+++ controllers/sudoku-original-solver.js
@@ -0,0 +1,355 @@
+/*
+ SudokuSolver Class for solving a sudoku puzzle. Input is an 81 character
+ string representing the initial state of the board.
+
+ Valid characters are 1-9 and period for no value.
+
+ Example:
+ '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.'
+ */
+
+// Utility set operations for finding values
+const completedSet = new Set([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+// symmetricDifference function copied from:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+const symmetricDifference = (setA, setB) => {
+  let _difference = new Set(setA);
+  for (let elem of setB) {
+    if (_difference.has(elem)) {
+      _difference.delete(elem);
+    } else {
+      _difference.add(elem);
+    }
+  }
+  return _difference;
+};
+
+const isValidValue = (valueSet, value) => {
+  const values = new Set(valueSet);
+  values.delete(null);
+  const validValues = symmetricDifference(values, completedSet);
+  return validValues.has(value);
+};
+
+/*
+ * The sudoku Board is a 3 dimensional array comprised of 9 matrices.
+ * Each matrix represents a region of the sudoku board. Regions start
+ * at the top left of the board.
+ *
+ * All indexing is 0 based
+ *
+ * Solving Algorithm is:
+ * find a cell with the least amount of valid values.
+ * If the cell has no valid values, backtrack to the previous move with at
+ * least one valid value and continue.
+ * If there are no previous moves with valid values, the puzzle layout is
+ * unsolvable.
+ * If there is a valid value, set the cell to a valid value.
+ * repeat until the puzzle is either solved, or unsolvable.
+ *
+ * The deterministic flag determines if we use .pop() to get the current cell
+ * and value or if we randomly pick a cell and value
+ *
+ * The verbose flag logs the state of the solving process
+*/
+
+class SudokuSolver {
+  constructor(puzzleString = null, deterministic = false, verbose = false) {
+    this.sudokuBoard =
+      [
+        [[null, null, null], [null, null, null], [null, null, null]],
+        [[null, null, null], [null, null, null], [null, null, null]],
+        [[null, null, null], [null, null, null], [null, null, null]],
+        [[null, null, null], [null, null, null], [null, null, null]],
+        [[null, null, null], [null, null, null], [null, null, null]],
+        [[null, null, null], [null, null, null], [null, null, null]],
+        [[null, null, null], [null, null, null], [null, null, null]],
+        [[null, null, null], [null, null, null], [null, null, null]],
+        [[null, null, null], [null, null, null], [null, null, null]],
+      ];
+
+    if (!!puzzleString) {
+      this.setBoardFromString(puzzleString);
+    }
+    this.deterministic = deterministic;
+    this.verbose = verbose;
+  }
+
+  // The index functions take a value between 0 - 80 and return the index
+  // for the appropriate object in the sudoku board array
+  regionIndex(i) {
+    return (Math.floor(i / 27) * 3) + Math.floor((i % 27) / 3) % 3;
+  };
+
+  regionRowIndex(i) {
+    return Math.floor((i % 27) / 9);
+  };
+
+  regionRowCellIndex(i) {
+    return (i % 9) % 3;
+  };
+
+  boardRowIndex(i) {
+    return Math.floor(i / 9);
+  }
+
+  boardColumnIndex(i) {
+    return i % 9;
+  }
+
+  // The get functions return a specific object from the board.
+  // These are the entire board, a region, a column, or a row.
+  getBoard() {
+    return this.sudokuBoard;
+  }
+
+  getBoardRegion(row, column) {
+    const indexNum = (row * 9) + column;
+    return this.sudokuBoard[this.regionIndex(indexNum)].flat();
+  };
+
+  getBoardRow(row) {
+    const result = [];
+    const indexNum = row * 9;
+    const indexRow = this.regionRowIndex(indexNum);
+    const indexSquare = this.regionIndex(indexNum);
+    for (let offset = 0; offset < 3; offset++) {
+      result.push(this.sudokuBoard[indexSquare + offset][indexRow]);
+    }
+    return result.flat();
+  };
+
+  getBoardColumn(column) {
+    const result = [];
+    for (let row = 0; row < 9; row++) {
+      const index = (row * 9) + column;
+      result.push(
+        this.sudokuBoard[this.regionIndex(index)]
+          [this.regionRowIndex(index)]
+          [this.regionRowCellIndex(index)]);
+    }
+    return result;
+  };
+
+  // Cell operations, get, set, and remove
+  getCell(row, column) {
+    const index = (row * 9) + column;
+    return this.sudokuBoard[this.regionIndex(index)]
+      [this.regionRowIndex(index)]
+      [this.regionRowCellIndex(index)];
+  }
+
+  setCell(row, column, value) {
+    const index = (row * 9) + column;
+    if (/[1-9]/.test(value) || value === null) {
+      this.sudokuBoard[this.regionIndex(index)]
+        [this.regionRowIndex(index)]
+        [this.regionRowCellIndex(index)] = !!value ? +value : null;
+    }
+    return this.getCell(row, column);
+  }
+
+  removeCell(row, column) {
+    const index = (row * 9) + column;
+    this.sudokuBoard[this.regionIndex(index)]
+      [this.regionRowIndex(index)]
+      [this.regionRowCellIndex(index)] = null;
+  }
+
+  // Boolean logic functions to test validity of input and
+  // placement on the board
+  isValidPuzzleString(puzzleString) {
+    return this.isValidLength(puzzleString) &&
+      this.isValidCharacters(puzzleString);
+  }
+
+  isValidLength(puzzleString) {
+    return (puzzleString.length === 81);
+  }
+
+  isValidCharacters(puzzleString) {
+    const puzzleCharacterRegEx = /^[.1-9]+$/;
+    return puzzleCharacterRegEx.test(puzzleString);
+  }
+
+  isValidRowPlacement(row, column, value) {
+    if (this.getCell(row, column)) {
+      return false;
+    } else {
+      return isValidValue(this.getBoardRow(row), value);
+    }
+  }
+
+  isValidColumnPlacement(row, column, value) {
+    if (this.getCell(row, column)) {
+      return false;
+    } else {
+      return isValidValue(this.getBoardColumn(column), value);
+    }
+  }
+
+  isValidRegionPlacement(row, column, value) {
+    if (this.getCell(row, column)) {
+      return false;
+    } else {
+      return isValidValue(this.getBoardRegion(row, column), value);
+    }
+  }
+
+  isValidBoard() {
+    for (let i = 0; i < 81; i++) {
+      const row = this.boardRowIndex(i);
+      const column = this.boardColumnIndex(i);
+      const cellValue = this.getCell(row, column);
+      const validValues = this.getValidPossibleValues(row, column, true);
+      if (cellValue && !validValues.has(cellValue)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  isBoardSolved() {
+    const diff = symmetricDifference(this.getBoard().flat(2), completedSet);
+    return this.isValidBoard() && diff.size === 0;
+  }
+
+  // Get a set of the valid values for a specific cell at a specified
+  // row and column coordinate
+  getValidPossibleValues(row, column, removeCurrentValue = false) {
+    const cellValue = this.getCell(row, column);
+
+    if (removeCurrentValue) {
+      this.removeCell(row, column);
+    } else if (!!cellValue) {
+      return new Set([cellValue]);
+    }
+
+    const currentValues = [];
+    currentValues.push(this.getBoardRegion(row, column));
+    currentValues.push(this.getBoardRow(row));
+    currentValues.push(this.getBoardColumn(column));
+    const currentSet = new Set(currentValues.flat());
+    currentSet.delete(null);
+    const possibleValues = symmetricDifference(currentSet, completedSet);
+    // Restore the previous value
+    this.setCell(row, column, cellValue);
+    return possibleValues;
+  };
+
+  // Solve the current board, return a string representing the solved board
+  solve() {
+
+    // Verify that the initial state is a valid board
+    if (!this.isValidBoard()) {
+      return 'invalid board';
+    }
+
+    // Keep a stack of our attempts to solve, so we can backtrack as necessary
+    const attemptStack = [];
+    let attemptCount = 0;
+
+    while (!this.isBoardSolved()) {
+      attemptCount++;
+
+      // find the cells with the least number of possible values
+      // maximum number of valid values is 9
+      let cells = [];
+      let numberOfValidValues = 9;
+      let currentValidValues = new Set();
+      for (let index = 0; index < 81; index += 1) {
+        const row = this.boardRowIndex(index);
+        const column = this.boardColumnIndex(index);
+        const cellValue = this.getCell(row, column);
+        if (!!cellValue) continue;
+        currentValidValues = this.getValidPossibleValues(row, column);
+        if (currentValidValues.size <= numberOfValidValues) {
+          numberOfValidValues = currentValidValues.size;
+          cells = cells.filter(e => {
+            return e.values.length === numberOfValidValues;
+          });
+          cells.push({
+            row: row,
+            column: column,
+            values: [...currentValidValues],
+          });
+        }
+      }
+
+      // Set the cell to a valid cell
+      let cell;
+      if (this.deterministic) {
+        cell = cells.pop();
+      } else {
+        cell = cells[Math.floor(Math.random() * cells.length)];
+      }
+
+      // if we have no valid values, backtrack
+      if (cell.values.length === 0 && attemptStack.length > 0) {
+        while (attemptStack.length > 0) {
+          cell = attemptStack.pop();
+          if (cell.values.length > 0) break;
+        }
+        this.setBoardFromString(cell.string);
+      }
+
+      // If we have no valid values and stack is empty, puzzle is unsolvable
+      if (cell.values.length === 0 && attemptStack.length === 0) {
+        if (this.verbose) console.log(cell);
+        return 'Unsolvable puzzle layout';
+      }
+
+      let valueIndex;
+      if (this.deterministic) {
+        cell.setValue = cell.values.pop();
+      } else {
+        valueIndex = Math.floor(Math.random() * cell.values.length);
+        cell.setValue = cell.values[valueIndex];
+        cell.values.splice(valueIndex, 1);
+      }
+      this.setCell(cell.row, cell.column, cell.setValue);
+      cell.string = this.getStringFromBoard();
+      attemptStack.push(cell);
+
+      if (attemptCount === 5000) {
+        if (this.verbose) console.log(attemptStack);
+        return 'timeout attempting to solve';
+      }
+      if (this.verbose) {
+        console.log('Count:', attemptCount, 'Stack:', attemptStack.length,
+          this + '');
+      }
+    }
+    return this.getStringFromBoard();
+  }
+
+  // Set the board from a string and get a string from the board
+  setBoardFromString(puzzleString) {
+    if (this.isValidPuzzleString(puzzleString)) {
+      puzzleString.split('').forEach((d, i) => {
+        this.sudokuBoard[this.regionIndex(i)]
+          [this.regionRowIndex(i)]
+          [this.regionRowCellIndex(i)] = d !== '.' ? +d : null;
+      });
+    }
+  };
+
+  getStringFromBoard() {
+    const result = [];
+    for (let index = 0; index < 81; index++) {
+      const value = this.sudokuBoard[this.regionIndex(index)]
+        [this.regionRowIndex(index)]
+        [this.regionRowCellIndex(index)];
+      result.push(!!value ? value : '.');
+    }
+    return result.join('');
+  };
+
+  toString() {
+    return this.getStringFromBoard();
+  }
+}
+
+module.exports = SudokuSolver;
+
--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -27,196 +27,112 @@ const symmetricDifference = (setA, setB) => {
 
 const isValidValue = (valueSet, value) => {
   const values = new Set(valueSet);
-  values.delete(null);
+  values.delete(0);
   const validValues = symmetricDifference(values, completedSet);
   return validValues.has(value);
 };
 
 /*
- * The sudoku Board is a 3 dimensional array comprised of 9 matrices.
- * Each matrix represents a region of the sudoku board. Regions start
- * at the top left of the board.
+ * The sudoku Board is a 2 dimensional array comprised of 9 rows with 9 columns
  *
  * All indexing is 0 based
  *
- * Solving Algorithm is:
- * find a cell with the least amount of valid values.
- * If the cell has no valid values, backtrack to the previous move with at
- * least one valid value and continue.
- * If there are no previous moves with valid values, the puzzle layout is
- * unsolvable.
- * If there is a valid value, set the cell to a valid value.
- * repeat until the puzzle is either solved, or unsolvable.
+ * Solving Algorithm is a heuristic, recursive brute force algorithm
  *
- * The deterministic flag determines if we use .pop() to get the current cell
- * and value or if we randomly pick a cell and value
+ * The deterministic flag is left for backwards compatibility with the original algorithm
+ * It is unused in this solver.
  *
- * The verbose flag logs the state of the solving process
-*/
+ * The verbose flag is unused but left for backwards compatibility
+ */
 
 class SudokuSolver {
   constructor(puzzleString = null, deterministic = false, verbose = false) {
     this.sudokuBoard =
       [
-        [[null, null, null], [null, null, null], [null, null, null]],
-        [[null, null, null], [null, null, null], [null, null, null]],
-        [[null, null, null], [null, null, null], [null, null, null]],
-        [[null, null, null], [null, null, null], [null, null, null]],
-        [[null, null, null], [null, null, null], [null, null, null]],
-        [[null, null, null], [null, null, null], [null, null, null]],
-        [[null, null, null], [null, null, null], [null, null, null]],
-        [[null, null, null], [null, null, null], [null, null, null]],
-        [[null, null, null], [null, null, null], [null, null, null]],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
       ];
-
     if (!!puzzleString) {
       this.setBoardFromString(puzzleString);
     }
-    this.deterministic = deterministic;
-    this.verbose = verbose;
   }
 
-  // The index functions take a value between 0 - 80 and return the index
-  // for the appropriate object in the sudoku board array
-  regionIndex(i) {
-    return (Math.floor(i / 27) * 3) + Math.floor((i % 27) / 3) % 3;
-  };
-
-  regionRowIndex(i) {
-    return Math.floor((i % 27) / 9);
-  };
-
-  regionRowCellIndex(i) {
-    return (i % 9) % 3;
-  };
-
-  boardRowIndex(i) {
-    return Math.floor(i / 9);
-  }
-
-  boardColumnIndex(i) {
-    return i % 9;
-  }
-
-  // The get functions return a specific object from the board.
-  // These are the entire board, a region, a column, or a row.
-  getBoard() {
-    return this.sudokuBoard;
-  }
-
-  getBoardRegion(row, column) {
-    const indexNum = (row * 9) + column;
-    return this.sudokuBoard[this.regionIndex(indexNum)].flat();
-  };
-
-  getBoardRow(row) {
-    const result = [];
-    const indexNum = row * 9;
-    const indexRow = this.regionRowIndex(indexNum);
-    const indexSquare = this.regionIndex(indexNum);
-    for (let offset = 0; offset < 3; offset++) {
-      result.push(this.sudokuBoard[indexSquare + offset][indexRow]);
+  // setBoardFromString initializes the sudokuBoard with the given puzzle
+  setBoardFromString(puzzleString) {
+    if (this.isValidPuzzleString(puzzleString)) {
+      puzzleString.split('').forEach((d, i) => {
+        const row = Math.floor(i / 9);
+        const col = i % 9;
+        this.sudokuBoard[row][col] = d !== '.' ? +d : 0;
+      });
     }
-    return result.flat();
-  };
+  }
 
-  getBoardColumn(column) {
+  // getStringFromBoard returns a string representing the current state of the sudokuBoard
+  getStringFromBoard() {
     const result = [];
     for (let row = 0; row < 9; row++) {
-      const index = (row * 9) + column;
-      result.push(
-        this.sudokuBoard[this.regionIndex(index)]
-          [this.regionRowIndex(index)]
-          [this.regionRowCellIndex(index)]);
+      for (let column = 0; column < 9; column++) {
+        const value = this.sudokuBoard[row][column];
+        result.push(!!value ? value : '.');
+      }
     }
-    return result;
-  };
+    return result.join('');
+  }
 
-  // Cell operations, get, set, and remove
+  toString() {
+    return this.getStringFromBoard();
+  }
+
   getCell(row, column) {
-    const index = (row * 9) + column;
-    return this.sudokuBoard[this.regionIndex(index)]
-      [this.regionRowIndex(index)]
-      [this.regionRowCellIndex(index)];
+    return this.sudokuBoard[row][column];
   }
 
   setCell(row, column, value) {
-    const index = (row * 9) + column;
-    if (/[1-9]/.test(value) || value === null) {
-      this.sudokuBoard[this.regionIndex(index)]
-        [this.regionRowIndex(index)]
-        [this.regionRowCellIndex(index)] = !!value ? +value : null;
+    if (/[0-9]/.test(value)) {
+      this.sudokuBoard[row][column] = +value;
     }
     return this.getCell(row, column);
   }
 
   removeCell(row, column) {
-    const index = (row * 9) + column;
-    this.sudokuBoard[this.regionIndex(index)]
-      [this.regionRowIndex(index)]
-      [this.regionRowCellIndex(index)] = null;
+    this.sudokuBoard[row][column] = 0;
   }
 
-  // Boolean logic functions to test validity of input and
-  // placement on the board
-  isValidPuzzleString(puzzleString) {
-    return this.isValidLength(puzzleString) &&
-      this.isValidCharacters(puzzleString);
+  // getRow returns the corresponding row in the sudokuboard
+  getRow(row) {
+    return this.sudokuBoard[row];
   }
 
-  isValidLength(puzzleString) {
-    return (puzzleString.length === 81);
-  }
-
-  isValidCharacters(puzzleString) {
-    const puzzleCharacterRegEx = /^[.1-9]+$/;
-    return puzzleCharacterRegEx.test(puzzleString);
-  }
-
-  isValidRowPlacement(row, column, value) {
-    if (this.getCell(row, column)) {
-      return false;
-    } else {
-      return isValidValue(this.getBoardRow(row), value);
+  // getColumn returns the corresponding column in the sudokuboard
+  getColumn(column) {
+    const result = [];
+    for (let i = 0; i < 9; i++) {
+      result.push(this.sudokuBoard[i][column]);
     }
+    return result;
   }
 
-  isValidColumnPlacement(row, column, value) {
-    if (this.getCell(row, column)) {
-      return false;
-    } else {
-      return isValidValue(this.getBoardColumn(column), value);
-    }
-  }
-
-  isValidRegionPlacement(row, column, value) {
-    if (this.getCell(row, column)) {
-      return false;
-    } else {
-      return isValidValue(this.getBoardRegion(row, column), value);
-    }
-  }
-
-  isValidBoard() {
-    for (let i = 0; i < 81; i++) {
-      const row = this.boardRowIndex(i);
-      const column = this.boardColumnIndex(i);
-      const cellValue = this.getCell(row, column);
-      const validValues = this.getValidPossibleValues(row, column, true);
-      if (cellValue && !validValues.has(cellValue)) {
-        return false;
+  // getRegion returns the corresponding region in the sudokuboard identified by the row and column
+  getRegion(row, column) {
+    const result = [];
+    const startRow = Math.floor(row / 3) * 3;
+    const startColumn = Math.floor(column / 3) * 3;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        result.push(this.sudokuBoard[startRow + i][startColumn + j]);
       }
     }
-    return true;
+    return result;
   }
 
-  isBoardSolved() {
-    const diff = symmetricDifference(this.getBoard().flat(2), completedSet);
-    return this.isValidBoard() && diff.size === 0;
-  }
-
-  // Get a set of the valid values for a specific cell at a specified
-  // row and column coordinate
   getValidPossibleValues(row, column, removeCurrentValue = false) {
     const cellValue = this.getCell(row, column);
 
@@ -227,129 +143,131 @@ class SudokuSolver {
     }
 
     const currentValues = [];
-    currentValues.push(this.getBoardRegion(row, column));
-    currentValues.push(this.getBoardRow(row));
-    currentValues.push(this.getBoardColumn(column));
+    currentValues.push(this.getRegion(row, column));
+    currentValues.push(this.getRow(row));
+    currentValues.push(this.getColumn(column));
     const currentSet = new Set(currentValues.flat());
-    currentSet.delete(null);
+    currentSet.delete(0);
     const possibleValues = symmetricDifference(currentSet, completedSet);
     // Restore the previous value
     this.setCell(row, column, cellValue);
     return possibleValues;
   };
 
-  // Solve the current board, return a string representing the solved board
+  // isValidPuzzleString returns true if the string is a valid sudoku layout
+  isValidPuzzleString(puzzleString) {
+    return this.isValidLength(puzzleString) &&
+      this.isValidCharacters(puzzleString);
+  }
+
+  // isValidLength returns true if the string is 81 characters
+  isValidLength(puzzleString) {
+    return (puzzleString.length === 81);
+  }
+
+  // isValidCharacters returns true if the string only contains characters '1' - '9' and '.'
+  isValidCharacters(puzzleString) {
+    const puzzleCharacterRegEx = /^[.1-9]+$/;
+    return puzzleCharacterRegEx.test(puzzleString);
+  }
+
+  // isValidRowPlacement returns true if a value is a valid value for a row
+  isValidRowPlacement(row, column, value) {
+    if (this.getCell(row, column)) {
+      return false;
+    } else {
+      return isValidValue(this.getRow(row), value);
+    }
+  }
+
+  // isValidColumnPlacement returns true if a value is a valid value for a column
+  isValidColumnPlacement(row, column, value) {
+    if (this.getCell(row, column)) {
+      return false;
+    } else {
+      return isValidValue(this.getColumn(column), value);
+    }
+  }
+
+  // isValidRegionPlacement returns true if a value is a valid value for a region
+  isValidRegionPlacement(row, column, value) {
+    if (this.getCell(row, column)) {
+      return false;
+    } else {
+      return isValidValue(this.getRegion(row, column), value);
+    }
+  }
+
+  // isValidBoard returns true if the board satisfies sudoku constraints
+  isValidBoard() {
+    for (let row = 0; row < 9; row++) {
+      for (let column = 0; column < 9; column++) {
+        const cellValue = this.getCell(row, column);
+        const validValues = this.getValidPossibleValues(row, column, true);
+        if (cellValue && !validValues.has(cellValue)) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  isBoardSolved() {
+    const diff = symmetricDifference(this.sudokuBoard.flat(), completedSet);
+    return this.isValidBoard() && diff.size === 0;
+  }
+
+// Solve the current board, return a string representing the solved board
   solve() {
+    const getCellWithLeastValidValues = () => {
+      const result = {
+        'row': 9,
+        'column': 9,
+        'values': [],
+      };
+      let minCount = 9;
+
+      for (let row = 0; row < 9; row++) {
+        for (let column = 0; column < 9; column++) {
+          if (this.getCell(row, column) === 0) {
+            const values = this.getValidPossibleValues(row, column);
+            if (values.size < minCount) {
+              minCount = values.size;
+              result['row'] = row;
+              result['column'] = column;
+              result['values'] = [...values];
+            }
+          }
+        }
+      }
+      return result;
+    };
+
+    const heuristic_solver = (depth = 1) => {
+      if (this.isBoardSolved()) {
+        return true;
+      }
+      const cell = getCellWithLeastValidValues();
+      for (let i = 0; i < cell.values.length; i++) {
+        this.setCell(cell.row, cell.column, cell.values[i]);
+        if (heuristic_solver(depth + 1)) {
+          return true;
+        }
+        this.removeCell(cell.row, cell.column);
+      }
+      return false;
+    };
 
     // Verify that the initial state is a valid board
     if (!this.isValidBoard()) {
       return 'invalid board';
     }
-
-    // Keep a stack of our attempts to solve, so we can backtrack as necessary
-    const attemptStack = [];
-    let attemptCount = 0;
-
-    while (!this.isBoardSolved()) {
-      attemptCount++;
-
-      // find the cells with the least number of possible values
-      // maximum number of valid values is 9
-      let cells = [];
-      let numberOfValidValues = 9;
-      let currentValidValues = new Set();
-      for (let index = 0; index < 81; index += 1) {
-        const row = this.boardRowIndex(index);
-        const column = this.boardColumnIndex(index);
-        const cellValue = this.getCell(row, column);
-        if (!!cellValue) continue;
-        currentValidValues = this.getValidPossibleValues(row, column);
-        if (currentValidValues.size <= numberOfValidValues) {
-          numberOfValidValues = currentValidValues.size;
-          cells = cells.filter(e => {
-            return e.values.length === numberOfValidValues;
-          });
-          cells.push({
-            row: row,
-            column: column,
-            values: [...currentValidValues],
-          });
-        }
-      }
-
-      // Set the cell to a valid cell
-      let cell;
-      if (this.deterministic) {
-        cell = cells.pop();
-      } else {
-        cell = cells[Math.floor(Math.random() * cells.length)];
-      }
-
-      // if we have no valid values, backtrack
-      if (cell.values.length === 0 && attemptStack.length > 0) {
-        while (attemptStack.length > 0) {
-          cell = attemptStack.pop();
-          if (cell.values.length > 0) break;
-        }
-        this.setBoardFromString(cell.string);
-      }
-
-      // If we have no valid values and stack is empty, puzzle is unsolvable
-      if (cell.values.length === 0 && attemptStack.length === 0) {
-        if (this.verbose) console.log(cell);
-        return 'Unsolvable puzzle layout';
-      }
-
-      let valueIndex;
-      if (this.deterministic) {
-        cell.setValue = cell.values.pop();
-      } else {
-        valueIndex = Math.floor(Math.random() * cell.values.length);
-        cell.setValue = cell.values[valueIndex];
-        cell.values.splice(valueIndex, 1);
-      }
-      this.setCell(cell.row, cell.column, cell.setValue);
-      cell.string = this.getStringFromBoard();
-      attemptStack.push(cell);
-
-      if (attemptCount === 5000) {
-        if (this.verbose) console.log(attemptStack);
-        return 'timeout attempting to solve';
-      }
-      if (this.verbose) {
-        console.log('Count:', attemptCount, 'Stack:', attemptStack.length,
-          this + '');
-      }
+    if (heuristic_solver()) {
+      return this.getStringFromBoard();
+    } else {
+      return 'Unsolvable puzzle layout';
     }
-    return this.getStringFromBoard();
-  }
-
-  // Set the board from a string and get a string from the board
-  setBoardFromString(puzzleString) {
-    if (this.isValidPuzzleString(puzzleString)) {
-      puzzleString.split('').forEach((d, i) => {
-        this.sudokuBoard[this.regionIndex(i)]
-          [this.regionRowIndex(i)]
-          [this.regionRowCellIndex(i)] = d !== '.' ? +d : null;
-      });
-    }
-  };
-
-  getStringFromBoard() {
-    const result = [];
-    for (let index = 0; index < 81; index++) {
-      const value = this.sudokuBoard[this.regionIndex(index)]
-        [this.regionRowIndex(index)]
-        [this.regionRowCellIndex(index)];
-      result.push(!!value ? value : '.');
-    }
-    return result.join('');
-  };
-
-  toString() {
-    return this.getStringFromBoard();
   }
 }
 
 module.exports = SudokuSolver;
-
